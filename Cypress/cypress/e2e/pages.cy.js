ba392d12b65dbba173a90be0aed37d9f369336a5
David Escobar <de.escobarc1@uniandes.edu.co>
2024-11-10 17:26:39 -0500

    Feature crear page primer escenario pages

--- Cypress/cypress/e2e/pages.cy.js
+++ Cypress/cypress/e2e/pages.cy.js
@@ -1,6 +1,8 @@
 import { givenNavigateToTheSite, givenUserIsLogin } from '../steps/givenSteps.cy';
 import { whenNavigateToThePages } from '../steps/whenSteps.cy';
-import { thenCreateNewPage } from '../steps/thenSteps.cy';
+import { thenCreateNewPage, thenInsertTitlePage, thenInsertContentPage, thenClicInPublish, thenClicInFinishReview, thenClicInPublishPage,
+    thenCloseWindowPagePublished, thenViewCreatedPage
+ } from '../steps/thenSteps.cy';
 
 describe('Testing create pages ', () => {
     beforeEach(()=>{
@@ -10,11 +12,24 @@ describe('Testing create pages ', () => {
         givenUserIsLogin();
     });
 
-    it('Test create a new page', () => {
+    it('Como administrador inicio sesión, creo una página en Ghost exitosamente y la veo en el listado de páginas', () => {
         // When navego a la página de crear páginas
         whenNavigateToThePages();
         // And hago clic en crear nueva página
         thenCreateNewPage();
-
+        // And ingreso el título de la página "Titulo página"
+        thenInsertTitlePage("Titulo página");
+        // And ingreso el contenido de la página "Contenido de lá página" 
+        thenInsertContentPage("Contenido de lá página");
+        // And hago clic en Publish
+        thenClicInPublish();
+        // And hago clic en finalizar revisión
+        thenClicInFinishReview();
+        // And hago clic en Publish page
+        thenClicInPublishPage();
+        // And cierro la ventana de página publicada
+        thenCloseWindowPagePublished();
+        // And veo en el listado de páginas la página con el titulo "Titulo página"
+        thenViewCreatedPage("Titulo página");
     });
 });
--- Cypress/cypress/steps/thenSteps.cy.js
+++ Cypress/cypress/steps/thenSteps.cy.js
@@ -2,3 +2,44 @@ export function thenCreateNewPage() {
     cy.get('a[href="#/editor/page/"]').click();
     cy.wait(5000);
 }
+
+export function thenInsertTitlePage(title) {
+    cy.get('textarea[placeholder="Page title"]').type(title);
+    cy.wait(2000);
+}
+
+export function thenInsertContentPage(content) {
+    cy.get('.kg-prose').first().type(content);
+    cy.wait(2000);
+}
+
+export function thenClicInPublish() {
+    cy.get('button[data-test-button="publish-flow"]').first().click();
+    cy.wait(2000);
+}
+
+export function thenClicInFinishReview() {
+    cy.get('button[data-test-button="continue"]').first().click();
+    cy.wait(2000);
+}
+
+export function thenClicInPublishPage() {
+    cy.get('button[data-test-button="confirm-publish"]').first().click();
+    cy.wait(2000);
+}
+
+export function thenCloseWindowPagePublished() {
+    cy.get('button[data-test-button="close-publish-flow"]').first().click();
+    cy.wait(2000);
+}
+
+export function thenViewCreatedPage(title) {
+    var elementsFound = false;
+    cy.get("h3[class='gh-content-entry-title']").each(($el, index, $list) => {
+        const text = $el.innerText;
+        if (text === title) {
+            elementsFound = true;
+            expect(elementsFound).to.equal(true);
+        }
+    });
+}
