82e5f4b665d0ba6d87b278d509c9c16e83cd860a
Santiago Paeres <s.paeres10@uniandes.edu.co>
2024-11-10 18:05:46 -0500

    Video prendido

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  projectId: 'ghost-e2e',
+  projectId: "ghost-e2e",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  video: true,
 };
