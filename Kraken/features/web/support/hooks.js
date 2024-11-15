b86025ff0db426f149662d584bfe0578ddec948e
JuanSeromo-UniAndes <JuanSeromoAdmin@Juans-MacBook-Pro-2.local>
2024-11-15 12:38:37 -0500

    vrt config added

--- Kraken/features/web/support/hooks.js
+++ Kraken/features/web/support/hooks.js
@@ -1,30 +1,28 @@
 const { After, Before } = require('@cucumber/cucumber');
 const { WebClient } = require('kraken-node');
+const { ghostBaseUrl, ghostRcUrl, vrt } = require('../../../config.json')
+const {USERNAME1, PASSWORD1} = require('../../../properties.json')
+
+const url = vrt ? ghostRcUrl : ghostBaseUrl
 
 Before(async function() {
     this.deviceClient = new WebClient('chrome', {}, this.userId);
     this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
 
-    await this.driver.url("http://localhost:2368/ghost/#/signin");
+    await this.driver.url(url + "#/signin");
     await this.driver.pause(2000);
+
     const emailInput = await this.driver.$('input[name="identification"]');
-
     await emailInput.waitForExist({ timeout: 10000 })
-    await emailInput.waitForEnabled({ timeout: 10000 })
-    await emailInput.waitForDisplayed()
-
-    await emailInput.setValue("js.rodriguezm12345@uniandes.edu.co");
-    // await this.driver.pause(1000);
+    await emailInput.setValue(USERNAME1);
 
     const passwordInput = await this.driver.$('input[name="password"]');
-    await passwordInput.setValue('39443950dE*');
     await passwordInput.waitForExist()
-
-    // await this.driver.pause(1000);
+    await passwordInput.setValue(PASSWORD1);
 
     const loginButton = await this.driver.$('button[type="submit"]');
     await loginButton.click();
-    // await this.driver.pause(2000);
+    await this.driver.pause(3000);
 })
 
 After(async function() {
